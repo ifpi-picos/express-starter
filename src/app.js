9e5121338bc94ae397b33d80a59f722eb6f532fb
Jesiel Viana <jesielviana@proton.me>
2023-10-30 10:21:42 -0300

    aula de prisma

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import Express from 'express'; // importa o módulo do express
 import logger from 'morgan';
-import { primeiro, segundo } from './middlewares/main.js';
 import rotasDeUsuarios from './rotas/rotasDeUsuarios.js';
 
 // criar um instância/aplicação Express
 const app = Express();
-
-app.use(primeiro);
-app.use(segundo);
+// middleware para converter os body da requisição para json
+app.use(Express.json());
+// app.use(primeiro);
+// app.use(segundo);
 
 app.use(logger('dev'));
 
--- src/rotas/rotasDeUsuarios.js
+++ src/rotas/rotasDeUsuarios.js
@@ -1,17 +1,26 @@
+import { PrismaClient } from '@prisma/client';
 import Express from 'express';
 
 const router = Express.Router();
+const prisma = new PrismaClient();
 
-router.get('/', (req, res) => {
-  res.send('Get usuários.');
+router.get('/', async (req, res) => {
+  const usuarios = await prisma.usuario.findMany({});
+  res.send(usuarios);
 });
 
-router.post('/', (req, res) => {
-  res.send('Post usuário!');
+router.post('/', async (req, res) => {
+  const usuario = req.body;
+  await prisma.usuario.create({
+    data: usuario,
+  });
+  res.status(201).send('Usuário salvo com sucesso!');
 });
+
 router.put('/:id', (req, res) => {
   res.send('Put usuário!');
 });
+
 router.delete('/:id', (req, res) => {
   res.send('Delete usuário!');
 });
